65ab52be1132a1dafb7afb9ab09b1b736aec45a6
Heber Sandoval <he3ers@gmail.com>
2017-09-20 13:48:18 -0400

    Write test for controller.

--- js/app/controllers/MainController.js
+++ js/app/controllers/MainController.js
@@ -1,7 +1,13 @@
 function MainController() {
-    this.name = 'Bill Gates';
+    this.name = 'Steve Jobs';
 }
 
 angular
     .module('app')
     .controller('MainController', MainController);
+
+
+//testing test
+function addNumbers(numOne, numTwo) {
+  return parseFloat(numOne, 10) + parseFloat(numTwo, 10);
+}
